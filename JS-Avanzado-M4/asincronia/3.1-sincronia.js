c37da7050446e8bf2b1acd603d016910be55b6a7
Baterik <erik_bonou2@hotmail.com>
2023-09-21 20:22:54 -0600

    ejempolos de sincronia

--- JS-Avanzado-M4/asincronia/3.1-sincronia.js
+++ JS-Avanzado-M4/asincronia/3.1-sincronia.js
@@ -6,10 +6,10 @@
 
 class Sincronia{
     ejecucion(){
-        console.log("========== BLOQUE 1==========");
-        console.log("Primer proceso");
-        console.log("Segundo proceso");
-        console.log("Tercer proceso");
+        console.log("========== BLOQUE 1 ==========");
+        console.log("Primer proceso de 1");
+        console.log("Segundo proceso de 1");
+        console.log("Tercer proceso de 1");
     }
 }
 
