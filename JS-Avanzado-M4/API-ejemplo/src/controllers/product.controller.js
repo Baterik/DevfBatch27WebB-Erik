31b9a29baebb9ca81a5cbf3ba8b60cb31182ff80
Baterik <erik_bonou2@hotmail.com>
2023-10-10 18:21:32 -0600

    Actualización de verbos API

--- JS-Avanzado-M4/API-ejemplo/src/controllers/product.controller.js
+++ JS-Avanzado-M4/API-ejemplo/src/controllers/product.controller.js
@@ -1,7 +1,7 @@
 import { methods as productServices } from "../services/product.service";
 
 const getAllProducts = async(req, res)=>{
-    try{
+    try{        
         const sentencia = await productServices.readAllProducts(res); //*PIDE A LA BASE DE DATOS TODOS PARA TRAER TODOS LOS PRODUCTOS
         return sentencia
     } catch (error){
@@ -10,6 +10,64 @@ const getAllProducts = async(req, res)=>{
     }
 }
 
-export const methods = {
-    getAllProducts
+//C create es un verbo POST en una API restful
+const postProduct = async(req, res) =>{
+    try {
+        const {sku, name, description, price, url_image} = req.body;
+        console.log("nuevo producto Controlador");
+        const query= await productServices.createProduct(sku,name,description,price,url_image,res)
+    } catch (error){
+        res.status(400);
+        res.send(error.message);
+        
+    }
 }
+
+// R read es un verbo get en una API restful
+const getProductBySku = async (req,res)=>{
+    try {
+        const query = await productServices.readProduct(req.params,res)
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
+
+// U update
+const updateProduct = async (req,res)=>{
+    try {
+        const {sku} = req.params
+        const {name, description,price,url_image} = req.body;
+        const query = await productServices.upteProduct(sku, name, description,price,url_image,res)
+        console.log("SKU:",sku)
+        console.log("Body:",req.body)
+
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
+
+// D delete
+const deleteProductBySku = async (req,res)=>{
+    try {
+        console.log("MONO")
+        const query = await productServices.deleteProduct(req.params,res);
+        return query
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+}
+
+
+export const methods = {
+    getAllProducts, 
+    postProduct,
+    getProductBySku,
+    updateProduct,
+    deleteProductBySku
+}
+
