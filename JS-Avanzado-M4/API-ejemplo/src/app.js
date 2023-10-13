1e18ce5962d4848e3988f2af95732d37c440c883
Baterik <erik_bonou2@hotmail.com>
2023-10-12 19:03:49 -0600

    Actualización al cambiode puerto

--- JS-Avanzado-M4/API-ejemplo/src/app.js
+++ JS-Avanzado-M4/API-ejemplo/src/app.js
@@ -2,18 +2,19 @@ import express from "express";
 import morgan from "morgan";
 
 import productRoutes from "./routes/index.routes"
+import { PORT } from "./common/constants";
 
 const app = express();
 
 /**Se setea el puerto en el que correra nuestro server */
-app.set("port",4000);
+app.set("port",PORT);
 
 /**Declaramos que hacemos uso de Middlewares */
 app.use(morgan("dev"));
 app.use(express.json());
 
 /** Importamos nuestras rutas de la API (endpoints) de products.routes */
-/** http://127.0.0.1:4000/api-restful/products */
+/** http://127.0.0.1:5000/api-restful/products */
 app.use("/api-restful/products", productRoutes);
 
 export default app;
